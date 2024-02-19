692c32b7f6b2b26acb5255bec17db1bbabd5caec
Vladimir Lobko <vova1234lobko@gmail.com>
2024-02-19 13:51:37 +0300

    Add heading for main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.guest')
 @section('content')
-    Main Page
+    <div class="flex flex-col">
+        <h1 class="text-4xl uppercase text-center font-normal">Join in making history</h1>
+        <div class="text-lg text-center font-normal">Art store</div>
+    </div>
 @endsection
